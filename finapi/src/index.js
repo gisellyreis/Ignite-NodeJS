da31a03d1b6543997d806a9d420bc42a33a80bcc
gisellyareis <gisellyareis@gmail.com>
2022-12-19 15:51:29 -0300

    Revert "complete - 1st nodejs project"
    This reverts commit fd412646083e327f1d1acf4082e01be4a57c0a01.

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,4 +1,3 @@
-const { response } = require("express")
 const express = require("express")
 const { v4: uuidv4 } = require("uuid")
 
@@ -20,19 +19,6 @@ function verifyIfExistsAccountCPF(request, response, next) {
   request.customer = customer
 
   return next()
-}
-
-function getBalance(statement) {
-  const balance = statement.reduce((acc, operation) => {
-    if(operation.type === 'credit') {
-      return acc + operation.amount
-    }
-    else {
-      return acc - operation.amount
-    }
-  }, 0)
-
-  return balance
 }
 
 /**
@@ -65,88 +51,6 @@ app.post("/account", (request, response) => {
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request
   return response.json(customer.statement)
-})
-
-app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
-  const { description, amount } = request.body
-  const { customer } = request
-
-  const statementOperation = {
-    description,
-    amount,
-    created_at: new Date(),
-    type: "credit"
-  }
-
-  customer.statement.push(statementOperation)
-
-  return response.status(201).send()
-})
-
-app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
-  const { amount } = request.body
-  const { customer } = request
-
-  const balance = getBalance(customer.statement)
-
-  if(balance < amount) {
-    return response.status(400).json({ error: "Insufficient funds!" })
-  }
-
-  const statementOperation = {
-    amount,
-    created_at: new Date(),
-    type: "debit"
-  }
-
-  customer.statement.push(statementOperation)
-
-  return response.status(201).send()
-})
-
-app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
-  const { customer } = request
-  const { date } = request.query
-
-  const dateFormat = new Date(date + " 00:00")
-
-  const statement = customer.statement.filter(
-    (statement) =>
-      statement.created_at.toDateString() === 
-      new Date(dateFormat).toDateString()
-  )
-  
-  return response.json(statement)
-})
-
-app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
-  const { name } = request.body
-  const { customer } = request
-
-  customer.name = name
-  return response.status(201).send()
-})
-
-app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
-  const { customer } = request
-
-  return response.json(customer)
-})
-
-app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
-  const { customer } = request
-
-  customers.splice(customer, 1)
-
-  return response.status(200).json(customers)
-})
-
-app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
-  const { customer } = request
-
-  const balance = getBalance(customer.statement)
-
-  return response.json(balance)
 })
 
 
